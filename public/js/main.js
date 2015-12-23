8bc3fd9f8630a1fbabe00e2c5bf6025e17eb0c00
David Kirwan <davidkirwanirl@gmail.com>
2015-12-23 00:55:32 +0000

    - Fleshing out the basic functionality

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,19 @@ function ajax_post(){
   var disablebutton = "paste_button";
   $("#"+disablebutton).addClass("disabled");
 
-  var tmp = $('#paste_input').val();
-  var data = { paste_input : tmp }
+  var title = $('#paste_title').val();
+  var language = $('#paste_language').val();
+  var paste = $('#paste_input').val();
+  var data = { paste_input : paste,
+               paste_language : language,
+               paste_title : title };
 
   $.ajax({
     type: "POST",
     url: "/paste",
     data: data,
     success: function(data){
-      var theData = JSON.parse(data)
+      var theData = JSON.parse(data);
       console.log(theData);
       var divtoupdate = "result";
       var enablebutton = "post_button";
@@ -21,10 +25,36 @@ function ajax_post(){
   });
 }
 
+function empty_title(){
+  var title = $('#paste_title').val();
+  
+  if(title === "Enter title"){
+    $("#paste_title").val("");
+  }
+  else{
+    ;
+  }
+}
+
 function empty_box(){
-  $("#paste_input").val("");
+  var text = $('#paste_input').val();
+
+  if(text === "Enter text"){
+    $("#paste_input").val("");
+  }
+  else{
+    ;
+  }
 }
 
 function onSuccess(data){
   ;
+}
+
+function edit_paste(id){
+  console.log("Edit:" + id);
+}
+
+function delete_paste(id){
+  console.log("Delete: " + id);
 }
--- sinatra_paste.rb
+++ sinatra_paste.rb
@@ -10,6 +10,7 @@ require 'sinatra/base'
 require 'logger'
 require 'json'
 require 'puma'
+require 'tilt/erb'
 require File.join(File.dirname(__FILE__), '/lib/paste')
 
 
@@ -37,13 +38,15 @@ class App < Sinatra::Base
   
   # Pastes array
   set :the_pastes, [ 
-		     {:date=>Time.now.utc - 5000, :paste=>"some paste 5000"}, 
-		     {:date=>Time.now.utc - 4000, :paste=>"some paste 4000"},
-		     {:date=>Time.now.utc - 3000, :paste=>"some paste 3000"},
-		     {:date=>Time.now.utc - 2000, :paste=>"some paste 2000"},
-		     {:date=>Time.now.utc - 1000, :paste=>"some paste 1000"},
-		     {:date=>Time.now.utc, :paste=>"some paste latest"}
+		     {:date=>Time.now.utc - 5000, :title=>"title 5", :language=>"4", :paste=>"some paste 5000", :id=>1000}, 
+		     {:date=>Time.now.utc - 4000, :title=>"title 4", :language=>"1", :paste=>"some paste 4000", :id=>1001},
+		     {:date=>Time.now.utc - 3000, :title=>"title 3", :language=>"2", :paste=>"some paste 3000", :id=>1002},
+		     {:date=>Time.now.utc - 2000, :title=>"title 2", :language=>"2", :paste=>"some paste 2000", :id=>1003},
+		     {:date=>Time.now.utc - 1000, :title=>"title 1", :language=>"3", :paste=>"some paste 1000", :id=>1004},
+		     {:date=>Time.now.utc, :title=>"Latest title", :language=>"4", :paste=>"some paste latest", :id=> 1005}
 		   ]
+  set :languages, {"1"=>"Text", "2"=>"Javascript", "3"=>"Python", "4"=>"Ruby"}
+  set :ids, 1005
 
 #########################################################################################################
 
@@ -57,11 +60,27 @@ class App < Sinatra::Base
   end
 
   post '/paste' do
-    throw :halt, [400, {"Content-Type" => "text/plain"},["400 Bad Request"]] unless not params["paste_input"].nil? 
-    settings.log.debug "params: " + params.inspect
-    paste = {:date=>Time.now.utc, :paste=>params["paste_input"]}
+    throw :halt, [400, {"Content-Type" => "text/plain"}, ["400 Bad Request"]] unless The::App.check_params(params)
+    settings.ids += 1
+    paste = { :date=>Time.now.utc, 
+              :title=>params["paste_title"], 
+              :paste=>params["paste_input"], 
+              :language=>params["paste_language"], 
+              :id=>settings.ids }
     settings.the_pastes << paste
     paste.to_json
+  end
+
+  def self.check_params(params)
+    settings.log.debug "params: " + params.inspect
+    
+    if params["paste_input"].nil? ||
+       params["paste_language"].nil? ||
+       params["paste_title"].nil?
+      return false
+    else
+      return true
+    end
   end
 
 end # End of the App class
